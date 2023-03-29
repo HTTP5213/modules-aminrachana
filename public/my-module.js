aaab2e304ed70b088c58a3f3373617b54d7d3adc
Rachana Amin <114316116+aminrachana@users.noreply.github.com>
2023-03-29 12:14:54 -0400

    Update my-module.js

--- public/my-module.js
+++ public/my-module.js
@@ -1,3 +1,5 @@
+// my-module.js should import theShire and buckland; it should export theShire and statement
+
 import { theShire, buckland } from './another-module.js';
 
 const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
